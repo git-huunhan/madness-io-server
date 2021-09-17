6bebf898802a63076467f99b2404f738112b8560
Huu Nhan <huunhankirigamer@gmail.com>
2021-09-17 15:42:49 +0700

    Fixed app.js

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const morgan = require("morgan");
 const { readdirSync } = require("fs");
 
 // Use .env variable
